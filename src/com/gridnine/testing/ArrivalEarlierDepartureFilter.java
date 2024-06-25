b79ff7a9cdf4c6238a857c8650bc466d3b87d820
VGAliyev <v.aliev84@yandex.ru>
2024-06-25 22:40:54 +0400

    ArrivalEarlierDepartureFilter complete

--- src/com/gridnine/testing/ArrivalEarlierDepartureFilter.java
+++ src/com/gridnine/testing/ArrivalEarlierDepartureFilter.java
@@ -1,10 +1,14 @@
 package com.gridnine.testing;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrivalEarlierDepartureFilter implements IFilter {
     @Override
     public List<Flight> doFilter(List<Flight> flightList) {
-        return List.of();
+        return flightList.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getArrivalDate().isAfter(segment.getDepartureDate())))
+                .collect(Collectors.toList());
     }
 }
