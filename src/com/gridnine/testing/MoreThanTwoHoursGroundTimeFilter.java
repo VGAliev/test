9361d7727e29bc92f2b1c56b510f958532b04be0
VGAliyev <v.aliev84@yandex.ru>
2024-06-25 22:54:45 +0400

    MoreThanTwoHoursGroundTimeFilter complete

--- src/com/gridnine/testing/MoreThanTwoHoursGroundTimeFilter.java
+++ src/com/gridnine/testing/MoreThanTwoHoursGroundTimeFilter.java
@@ -1,10 +1,23 @@
 package com.gridnine.testing;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MoreThanTwoHoursGroundTimeFilter implements IFilter {
     @Override
     public List<Flight> doFilter(List<Flight> flightList) {
-        return List.of();
+        return flightList.stream()
+                .filter(flight -> {
+                    List<Segment> segments = flight.getSegments();
+                    for (int i = 0; i < segments.size() - 1; i++) {
+                        Duration duration = Duration.between(segments.get(i).getArrivalDate(), segments.get(i + 1).getDepartureDate());
+                        if (duration.toHours() >= 2) {
+                            return false;
+                        }
+                    }
+                    return true;
+                })
+                .collect(Collectors.toList());
     }
 }
